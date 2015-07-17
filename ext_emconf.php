4a6d3c146d218e8385cf3ec4b634e65cd2e4964d
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2015-07-17 14:39:49 +0200

    [TASK] Cleanup and simplify code

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,16 +31,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'version' => '0.3.0',
 	'constraints' => array(
-		'depends' => array(
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
+		'depends' => array(),
+		'conflicts' => array(),
+		'suggests' => array(),
 	),
 	'_md5_values_when_last_written' => 'a:18:{s:9:"ChangeLog";s:4:"26c7";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"2f13";s:14:"ext_tables.php";s:4:"0de3";s:12:"flexform.xml";s:4:"b198";s:16:"locallang_db.xml";s:4:"7a06";s:31:"pi1/class.tx_ezbrequest_pi1.php";s:4:"041f";s:17:"pi1/item_view.htm";s:4:"f9ea";s:17:"pi1/list_view.htm";s:4:"a9b0";s:17:"pi1/locallang.xml";s:4:"d05d";s:11:"res/ezb.css";s:4:"f40f";s:13:"res/green.gif";s:4:"1365";s:11:"res/red.gif";s:4:"1e77";s:15:"res/unknown.gif";s:4:"e26a";s:14:"res/yellow.gif";s:4:"733c";s:18:"res/yellow_red.gif";s:4:"eb41";s:20:"static/constants.txt";s:4:"22f4";s:16:"static/setup.txt";s:4:"97f7";}',
-	'suggests' => array(
-	),
+	'suggests' => array(),
 );
 
 ?>
