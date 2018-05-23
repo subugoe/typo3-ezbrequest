691a583ece53e0dd204c18de8b717b008ef769d6
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2018-05-23 10:34:54 +0200

    Remove TCA from ext_tables.php

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['ezbrequest_pi1'] = 'layout,select_key';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['ezbrequest_pi1_pi1'] = 'pi_flexform';
